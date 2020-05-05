9efbbb656aac4d86c3e2aecb5babb58b008c7f40
Andreas Frömer <andreas.froemer@check24.de>
2020-05-05 09:56:14 +0200

    Add possibility for leading comma

--- tests/PHPStan/Parser/PhpDocParserTest.php
+++ tests/PHPStan/Parser/PhpDocParserTest.php
@@ -3078,6 +3078,54 @@ chunk. Must be higher that in the previous request.'),
 				),
 			]),
 		];
+
+//		yield [
+//			'multiline generic types - trailing comma',
+//			'/**' . PHP_EOL .
+//			' * @implements Foo<' . PHP_EOL .
+//			' *    A,' . PHP_EOL .
+//			' * >' . PHP_EOL .
+//			' */',
+//			new PhpDocNode([
+//				new PhpDocTagNode(
+//					'@implements',
+//					new ImplementsTagValueNode(
+//						new GenericTypeNode(
+//							new IdentifierTypeNode('Foo'),
+//							[
+//								new IdentifierTypeNode('A'),
+//							]
+//						),
+//						''
+//					)
+//				),
+//			]),
+//		];
+
+		yield [
+			'multiline generic types - leading comma',
+			'/**' . PHP_EOL .
+			' * @implements Foo<' . PHP_EOL .
+			' *    A' . PHP_EOL .
+			' *    , B' . PHP_EOL .
+			' * >' . PHP_EOL .
+			' */',
+			new PhpDocNode([
+				new PhpDocTagNode(
+					'@implements',
+					new ImplementsTagValueNode(
+						new GenericTypeNode(
+							new IdentifierTypeNode('Foo'),
+							[
+								new IdentifierTypeNode('A'),
+								new IdentifierTypeNode('B'),
+							]
+						),
+						''
+					)
+				),
+			]),
+		];
 	}
 
 	public function dataParseTagValue(): array
