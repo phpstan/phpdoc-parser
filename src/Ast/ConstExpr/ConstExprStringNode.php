f7d768cb3772f64ca7f1986da269df067184509b
Daniil Gentili <daniil@daniil.it>
2022-02-22 14:04:21 +0100

    Undo changes

--- src/Ast/ConstExpr/ConstExprStringNode.php
+++ src/Ast/ConstExpr/ConstExprStringNode.php
@@ -3,9 +3,6 @@
 namespace PHPStan\PhpDocParser\Ast\ConstExpr;
 
 use PHPStan\PhpDocParser\Ast\NodeAttributes;
-use function strlen;
-use function strpos;
-use function substr;
 
 class ConstExprStringNode implements ConstExprNode
 {
