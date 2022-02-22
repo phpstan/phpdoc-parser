d567bbbf8ac05b8641d5ad26382e2068e3039d8e
Daniil Gentili <daniil@daniil.it>
2022-02-22 14:03:58 +0100

    Undo changes

--- src/Ast/ConstExpr/ConstExprStringNode.php
+++ src/Ast/ConstExpr/ConstExprStringNode.php
@@ -17,19 +17,6 @@ class ConstExprStringNode implements ConstExprNode
 
 	public function __construct(string $value)
 	{
-		$len = strlen($value);
-		if ($len >= 2 && (
-			($value[0] === '"' && $value[$len - 1] === '"')
-			|| ($value[0] === "'" && $value[$len - 1] === "'")
-		)) {
-			$value = substr($value, 1, -1);
-		}
-		// Don't go crazy with escaping
-		if (strpos($value, '"') !== false) {
-			$value = "'" . $value . "'";
-		} else {
-			$value = '"' . $value . '"';
-		}
 		$this->value = $value;
 	}
 
