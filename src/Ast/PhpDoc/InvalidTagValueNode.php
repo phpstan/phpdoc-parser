34545bb30a6f8bd86cfa371dbd42140a657bbf0d
Ondrej Mirtes <ondrej@mirtes.cz>
2022-04-08 13:29:30 +0200

    Fix memory leak
    Exceptions in their stack traces carry references to all unexpected objects

--- src/Ast/PhpDoc/InvalidTagValueNode.php
+++ src/Ast/PhpDoc/InvalidTagValueNode.php
@@ -4,7 +4,13 @@ namespace PHPStan\PhpDocParser\Ast\PhpDoc;
 
 use PHPStan\PhpDocParser\Ast\NodeAttributes;
 use PHPStan\PhpDocParser\Parser\ParserException;
+use function sprintf;
+use function trigger_error;
+use const E_USER_WARNING;
 
+/**
+ * @property ParserException $exception
+ */
 class InvalidTagValueNode implements PhpDocTagValueNode
 {
 
@@ -13,15 +19,30 @@ class InvalidTagValueNode implements PhpDocTagValueNode
 	/** @var string (may be empty) */
 	public $value;
 
-	/** @var ParserException */
-	public $exception;
+	/** @var mixed[] */
+	private $exceptionArgs;
 
 	public function __construct(string $value, ParserException $exception)
 	{
 		$this->value = $value;
-		$this->exception = $exception;
+		$this->exceptionArgs = [
+			$exception->getCurrentTokenValue(),
+			$exception->getCurrentTokenType(),
+			$exception->getCurrentOffset(),
+			$exception->getExpectedTokenType(),
+			$exception->getExpectedTokenValue(),
+		];
 	}
 
+	public function __get(string $name)
+	{
+		if ($name !== 'exception') {
+			trigger_error(sprintf('Undefined property: %s::$%s', self::class, $name), E_USER_WARNING);
+			return null;
+		}
+
+		return new ParserException(...$this->exceptionArgs);
+	}
 
 	public function __toString(): string
 	{
