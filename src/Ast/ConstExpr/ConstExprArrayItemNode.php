61800f71a5526081d1b5633766aa88341f1ade76
Ondrej Mirtes <ondrej@mirtes.cz>
2022-12-20 21:51:49 +0100

    Fix ConstExprArrayItemNode::__toString()

--- src/Ast/ConstExpr/ConstExprArrayItemNode.php
+++ src/Ast/ConstExpr/ConstExprArrayItemNode.php
@@ -3,6 +3,7 @@
 namespace PHPStan\PhpDocParser\Ast\ConstExpr;
 
 use PHPStan\PhpDocParser\Ast\NodeAttributes;
+use function sprintf;
 
 class ConstExprArrayItemNode implements ConstExprNode
 {
@@ -25,11 +26,11 @@ class ConstExprArrayItemNode implements ConstExprNode
 	public function __toString(): string
 	{
 		if ($this->key !== null) {
-			return "{$this->key} => {$this->value}";
+			return sprintf('%s => %s', $this->key, $this->value);
 
 		}
 
-		return "{$this->value}";
+		return (string) $this->value;
 	}
 
 }
