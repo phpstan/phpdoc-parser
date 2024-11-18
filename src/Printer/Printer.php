fb19eedd2bb67ff8cf7a5502ad329e701d6398a3
Markus Staab <markus.staab@redaxo.de>
2024-11-18 16:42:48 +0100

    Support for `@pure-unless-parameter-passed`

--- src/Printer/Printer.php
+++ src/Printer/Printer.php
@@ -33,6 +33,7 @@ use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagValueNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTextNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PropertyTagValueNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PureUnlessCallableIsImpureTagValueNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\PureUnlessParameterIsPassedTagValueNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\RequireExtendsTagValueNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\RequireImplementsTagValueNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\ReturnTagValueNode;
@@ -352,6 +353,9 @@ final class Printer
 			return trim("{$node->type} {$node->parameterName} {$node->description}");
 		}
 		if ($node instanceof PureUnlessCallableIsImpureTagValueNode) {
+			return trim("{$node->parameterName} {$node->description}");
+		}
+		if ($node instanceof PureUnlessParameterIsPassedTagValueNode) {
 			return trim("{$node->parameterName} {$node->description}");
 		}
 		if ($node instanceof PropertyTagValueNode) {
